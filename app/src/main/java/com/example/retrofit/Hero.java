3e701a72b2b419a8f9f318dc28b6ed9d50c2e3e5
akanksha606 <akankshagupta98583@gmail.com>
2020-01-31 00:46:02 +0530

    Retrofit

---
+++ app/src/main/java/com/example/retrofit/Hero.java
@@ -0,0 +1,24 @@
+package com.example.retrofit;
+
+public class Hero {
+    private String name;
+    private String realname;
+    private String team;
+    private String firstappearance;
+    private String createdby;
+    private String publisher;
+    private String imageurl;
+    private String bio;
+
+    "name": "Captain America",
+            "realname": "Steve Rogers",
+            "team": "Avengers",
+            "firstappearance": "1941",
+            "createdby": "Joe Simon",
+            "publisher": "Marvel Comics",
+            "imageurl": "https://www.simplifiedcoding.net/demos/marvel/captainamerica.jpg",
+            "bio": "\r\n\t\tSteven Rogers was born in the Lower East Side of Manhattan," +
+            " New York City, in 1925 to poor Irish immigrants, Sarah and Joseph Rogers.[54] Joseph died when Steve was a child," +
+            " and Sarah died of pneumonia while Steve was a teen. By early 1940, before America's entry into World War II, " +
+            "Rogers is a tall, scrawny fine arts student specializing in illustration and a comic book writer and artist.\r\n\t\t"
+}
