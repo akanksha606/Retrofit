6940b721e84c195be0adce0daba9693c1b8c5bfa
akanksha606 <akankshagupta98583@gmail.com>
2020-02-01 00:55:43 +0530

    Retrofit

--- app/src/main/java/com/example/retrofit/Hero.java
+++ app/src/main/java/com/example/retrofit/Hero.java
@@ -10,15 +10,46 @@ public class Hero {
     private String imageurl;
     private String bio;
 
-    "name": "Captain America",
-            "realname": "Steve Rogers",
-            "team": "Avengers",
-            "firstappearance": "1941",
-            "createdby": "Joe Simon",
-            "publisher": "Marvel Comics",
-            "imageurl": "https://www.simplifiedcoding.net/demos/marvel/captainamerica.jpg",
-            "bio": "\r\n\t\tSteven Rogers was born in the Lower East Side of Manhattan," +
-            " New York City, in 1925 to poor Irish immigrants, Sarah and Joseph Rogers.[54] Joseph died when Steve was a child," +
-            " and Sarah died of pneumonia while Steve was a teen. By early 1940, before America's entry into World War II, " +
-            "Rogers is a tall, scrawny fine arts student specializing in illustration and a comic book writer and artist.\r\n\t\t"
+    public Hero(String name, String realname, String team, String firstappearance, String createdby, String publisher, String imageurl, String bio) {
+        this.name = name;
+        this.realname = realname;
+        this.team = team;
+        this.firstappearance = firstappearance;
+        this.createdby = createdby;
+        this.publisher = publisher;
+        this.imageurl = imageurl;
+        this.bio = bio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public String getFirstappearance() {
+        return firstappearance;
+    }
+
+    public String getCreatedby() {
+        return createdby;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public String getBio() {
+        return bio;
+    }
 }
--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.retrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ListView listView=findViewById(R.id.listView);
+        Retrofit retrofit=new Retrofit.Builder().baseUrl(Api.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        Api api=retrofit.create(Api.class);
+        Call<List<Hero>> call=api.getHeroes();
+        call.enqueue(new Callback<List<Hero>>() {
+            @Override
+            public void onResponse(Call<List<Hero>> call, Response<List<Hero>> response) {
+                  List<Hero> heroes=response.body();
+                  String[] heronames=new String[heroes.size()];
+
+                  for(int i=0;i<heroes.size();i++){
+                    heronames[i]=heroes.get(i).getName();
+                  }
+                  listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(),android.R.layout.activity_list_item,heronames));
+                  for(Hero h:heroes){
+                      Log.d("name",h.getName());
+                      Log.d("realname",h.getRealname());
+                      Log.d("imageurl",h.getImageurl());
+                  }
+            }
+
+            @Override
+            public void onFailure(Call<List<Hero>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),t.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
